e7060118302f5d3450174afdfa3012a97304a887
Mikael Chudinov <mikael@chudinov.net>
2016-03-13 02:08:51 +0100

    Add border.

--- AspMvcACSOwin/App_Start/Startup.Auth.cs
+++ AspMvcACSOwin/App_Start/Startup.Auth.cs
@@ -8,20 +8,18 @@ namespace AspMvcACSOwin
 {
     public partial class Startup
     {
-        private static string realm = ConfigurationManager.AppSettings["ida:Wtrealm"];
-        private static string adfsMetadata = ConfigurationManager.AppSettings["ida:ADFSMetadata"];
+        private static readonly string _realm = ConfigurationManager.AppSettings["ida:Wtrealm"];
+        private static readonly string _adfsMetadata = ConfigurationManager.AppSettings["ida:ADFSMetadata"];
 
         public void ConfigureAuth(IAppBuilder app)
         {
             app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
-
             app.UseCookieAuthentication(new CookieAuthenticationOptions());
-
             app.UseWsFederationAuthentication(
                 new WsFederationAuthenticationOptions
                 {
-                    Wtrealm = realm,
-                    MetadataAddress = adfsMetadata
+                    Wtrealm = _realm,
+                    MetadataAddress = _adfsMetadata
                 });
         }
     }
